b5fe2ab7e8e3b1d39de7eb65cde6467e9a0d0a8d
Josh Dechant <josh@codecrafting.org>
2024-06-20 16:52:24 -0400

    make a new instance of brotli each time

--- precompile/contracts/compress/compress.go
+++ precompile/contracts/compress/compress.go
@@ -11,9 +11,6 @@ import (
 
 type Compress struct {
 	precompile.StatefulPrecompiledContract
-
-	reader *brotli.Reader
-	writer *brotli.Writer
 }
 
 func NewCompress() *Compress {
@@ -21,19 +18,17 @@ func NewCompress() *Compress {
 		StatefulPrecompiledContract: precompile.NewStatefulPrecompiledContract(
 			abi.CompressABI,
 		),
-		reader: brotli.NewReader(nil),
-		writer: brotli.NewWriterLevel(nil, brotli.DefaultCompression),
 	}
 }
 
 func (c *Compress) Compress(ctx precompile.StatefulContext, input []byte) ([]byte, error) {
 	var buf bytes.Buffer
-	c.writer.Reset(&buf)
-	_, err := c.writer.Write(input)
+	writer := brotli.NewWriterLevel(&buf, brotli.DefaultCompression)
+	_, err := writer.Write(input)
 	if err != nil {
 		return nil, err
 	}
-	if err := c.writer.Close(); err != nil {
+	if err := writer.Close(); err != nil {
 		return nil, err
 	}
 	return buf.Bytes(), nil
@@ -41,8 +36,8 @@ func (c *Compress) Compress(ctx precompile.StatefulContext, input []byte) ([]byt
 
 func (c *Compress) Decompress(ctx precompile.StatefulContext, input []byte) ([]byte, error) {
 	var buf bytes.Buffer
-	c.reader.Reset(bytes.NewReader(input))
-	_, err := io.Copy(&buf, c.reader)
+	reader := brotli.NewReader(bytes.NewReader(input))
+	_, err := io.Copy(&buf, reader)
 	if err != nil {
 		return nil, err
 	}
